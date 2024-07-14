81bcf14691ab3cec9531792646626b9124e3ca89
MichaellDev1 <santuchomichael131@gmail.com>
2024-07-14 15:48:40 -0300

    feat: update my cv

--- src/components/Pdf/index.jsx
+++ src/components/Pdf/index.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Pdf () {
   return (
     <div className='w-full min-h-[100vh]'>
-      <iframe src='https://drive.google.com/file/d/1riEbfipW_jt_FJE1XlHCJEqaCZqSoWba/preview' width='100%' height='1150px' allow='autoplay' />
+      <iframe src='https://drive.google.com/file/d/1nu7DpfWTD57ktJkSVi32NYDyUl0g7Dpn/preview' width='100%' height='1150px' allow='autoplay' />
     </div>
   )
 }
